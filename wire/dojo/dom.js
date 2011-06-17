788598da12502a178af372ee50f0addb1df2d011
Brian Cavalier <brian@briancavalier.com>
2011-06-17 17:09:22 -0400

    Fix extra comma, and comment out unused unload facet--still debating whether it is useful or dangerous

--- wire/dojo/dom.js
+++ wire/dojo/dom.js
@@ -24,38 +24,38 @@ define(['dojo', 'wire/domReady'], function(dojo, domReady) {
 		});		
 	}
 
-	function unloadAspect(promise, facet, wire) {
-		var spec, unload, tunload, target;
+	// function unloadAspect(promise, facet, wire) {
+	// 	var spec, unload, tunload, target;
 
-		spec = facet.options;
-		unload = spec.unload;
-		tunload = typeof unload;
-		target = facet.target;
+	// 	spec = facet.options;
+	// 	unload = spec.unload;
+	// 	tunload = typeof unload;
+	// 	target = facet.target;
 	
-		// If it's an object, there may be more than one unload func to
-		// call, and each may have args.
-		// If it's just a string, then it's the name of a function to
-		// call on unload.
-		if(tunload == 'object') {
-			dojo.addOnUnload(function() {
-				for(var f in unload) {
-					facet.invoke(target[f], unload[f]);
-				}
-			});
+	// 	// If it's an object, there may be more than one unload func to
+	// 	// call, and each may have args.
+	// 	// If it's just a string, then it's the name of a function to
+	// 	// call on unload.
+	// 	if(tunload == 'object') {
+	// 		dojo.addOnUnload(function() {
+	// 			for(var f in unload) {
+	// 				facet.invoke(target[f], unload[f]);
+	// 			}
+	// 		});
 			
-		} else if(tunload == 'string') {
-			dojo.addOnUnload(function() {
-				facet.invoke(target[unload]);
-			});
-		}
-	}
+	// 	} else if(tunload == 'string') {
+	// 		dojo.addOnUnload(function() {
+	// 			facet.invoke(target[unload]);
+	// 		});
+	// 	}
+	// }
 	
 	return {
 		wire$plugin: function(ready, destroyed, options) {
 			return {
 				resolvers: {
 					'dom.query': resolveQuery
-				},
+				}//,
 				// facets: {
 				// 	unload: {
 				// 		initialized: unloadAspect
