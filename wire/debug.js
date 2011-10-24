965cd7af7b3d534bfe89c771977ec7d613e06ecf
Raul E Rangel <Raul.Rangel@disney.com>
2011-10-24 08:34:34 -0600

    Fixed Null pointer in IE and added stack traces
    In IE if you don't have the console open you will
    get NULL pointers when trying to access console.
    
    Also added stack traces to help in debugging.

--- wire/debug.js
+++ wire/debug.js
@@ -41,178 +41,197 @@
  *     filter: ".*"
  * }
  */
-define([], function() {
-	var timer, defaultTimeout;
+(function(global) {
+  define([], function() {
+    var timer, defaultTimeout;
 
-	timer = createTimer();
-	defaultTimeout = 5000; // 5 second wiring failure timeout
+    timer = createTimer();
+    defaultTimeout = 5000; // 5 second wiring failure timeout
 
-    /**
-     * Builds a string with timing info and a message for debug output
-     *
-     * @param text {String} message
-     * @param contextTimer per-context timer information
-     *
-     * @returns A formatted string for output
-     */
-	function time(text, contextTimer) {
-		var all = timer(),
-			timing = "(total: " +
-				(contextTimer
-					? all.total + "ms, context: " + contextTimer()
-					: all)
-				+ "): ";
-		return "DEBUG " + timing + text;
-	}
+      /**
+       * Builds a string with timing info and a message for debug output
+       *
+       * @param text {String} message
+       * @param contextTimer per-context timer information
+       *
+       * @returns A formatted string for output
+       */
+    function time(text, contextTimer) {
+      var all = timer(),
+        timing = "(total: " +
+          (contextTimer
+            ? all.total + "ms, context: " + contextTimer()
+            : all)
+          + "): ";
+      return "DEBUG " + timing + text;
+    }
 
-    /**
-     * Creates a timer function that, when called, returns an object containing
-     * the total elapsed time since the timer was created, and the split time
-     * since the last time the timer was called.  All times in milliseconds
-     *
-     * @returns timer
-     */
-	function createTimer() {
-		var start = new Date().getTime(),
-			split = start;
+      /**
+       * Creates a timer function that, when called, returns an object containing
+       * the total elapsed time since the timer was created, and the split time
+       * since the last time the timer was called.  All times in milliseconds
+       *
+       * @returns timer
+       */
+    function createTimer() {
+      var start = new Date().getTime(),
+        split = start;
 
-        /**
-         * Returns the total elapsed time since this timer was created, and the
-         * split time since this getTime was last called.
-         *
-         * @returns Object containing total and split times in milliseconds, plus a
-         * toString() function that is useful in logging the time.
-         */
-		return function getTime() {
-			var now, total, splitTime;
+          /**
+           * Returns the total elapsed time since this timer was created, and the
+           * split time since this getTime was last called.
+           *
+           * @returns Object containing total and split times in milliseconds, plus a
+           * toString() function that is useful in logging the time.
+           */
+      return function getTime() {
+        var now, total, splitTime;
 
-			now = new Date().getTime();
-			total = now - start;
-			splitTime = now - split;
-			split = now;
+        now = new Date().getTime();
+        total = now - start;
+        splitTime = now - split;
+        split = now;
 
-			return {
-				total: total,
-				split: splitTime,
-				toString: function() {
-					return '' + splitTime + 'ms / ' + total + 'ms';
-				}
-			};
-		};
-	}
+        return {
+          total: total,
+          split: splitTime,
+          toString: function() {
+            return '' + splitTime + 'ms / ' + total + 'ms';
+          }
+        };
+      };
+    }
 
-	function defaultFilter(path) {
-		return !!path;
-	}
+    function defaultFilter(path) {
+      return !!path;
+    }
 
-	return {
-		wire$plugin: function debugPlugin(ready, destroyed, options) {
-			var contextTimer, timeout, paths, logCreated, checkPathsTimeout, verbose, filter, filterRegex, plugin;
+    return {
+      wire$plugin: function debugPlugin(ready, destroyed, options) {
+        var contextTimer, timeout, paths, logCreated, checkPathsTimeout, verbose, filter, filterRegex, plugin;
 
-			verbose = options.verbose;
+        verbose = options.verbose;
 
-			if(options.filter) {
-				filterRegex = options.filter.test ? options.filter : new RegExp(options.filter);
-				filter = function(path) {
-					return filterRegex.test(path);
-				}
-			} else {
-				filter = defaultFilter;
-			}
+        if(options.filter) {
+          filterRegex = options.filter.test ? options.filter : new RegExp(options.filter);
+          filter = function(path) {
+            return filterRegex.test(path);
+          }
+        } else {
+          filter = defaultFilter;
+        }
 
-			contextTimer = createTimer();
+        contextTimer = createTimer();
 
-			function contextTime(msg) {
-				return time(msg, contextTimer);
-			}
+        function contextTime(msg) {
+          return time(msg, contextTimer);
+        }
 
-			console.log(contextTime("Context init"));
+        if (global.console && global.console.log) {
+          global.console.log(contextTime("Context init"));
+        }
 
-			ready.then(
-				function onContextReady(context) {
-					cancelPathsTimeout();
-					console.log(contextTime("Context ready"), context);
-				},
-				function onContextError(err) {
-					cancelPathsTimeout();
-					console.error(contextTime("Context ERROR: "), err);
-					console.error(err);
-				}
-			);
+        ready.then(
+          function onContextReady(context) {
+            cancelPathsTimeout();
+            if (global.console && global.console.log) {
+              global.console.log(contextTime("Context ready"), context);
+            }
+          },
+          function onContextError(err) {
+            cancelPathsTimeout();
+            if (global.console && global.console.error) {
+              global.console.error(contextTime("Context ERROR: "), err);
+              global.console.error(err);
+              global.console.error(err.stack);
+            }
+          }
+        );
 
-			destroyed.then(
-				function onContextDestroyed() {
-					console.log(contextTime("Context destroyed"));
-				},
-				function onContextDestroyError(err) {
-					console.error(contextTime("Context destroy ERROR"), err);
-				}
-			);
+        destroyed.then(
+          function onContextDestroyed() {
+            if (global.console && global.console.log) {
+              global.console.log(contextTime("Context destroyed"));
+            }
+          },
+          function onContextDestroyError(err) {
+            if (global.console && global.console.error) {
+              global.console.error(contextTime("Context destroy ERROR"), err);
+              global.console.error(err.stack);
+            }
+          }
+        );
 
-			function makeListener(step, verbose) {
-				return function(promise, proxy /*, wire */) {
-					var path = proxy.path;
+        function makeListener(step, verbose) {
+          return function(promise, proxy /*, wire */) {
+            var path = proxy.path;
 
-					if (path) {
-						paths[path].status = step;
-					}
+            if (path) {
+              paths[path].status = step;
+            }
 
-					if (verbose && filter(path)) {
-						var message = time(step + ' ' + (path || proxy.id || ''), contextTimer);
-						if (proxy.target) {
-							console.log(message, proxy.target, proxy.spec);
-						} else {
-							console.log(message, proxy);
-						}
-					}
+            if (verbose && filter(path)) {
+              var message = time(step + ' ' + (path || proxy.id || ''), contextTimer);
+              if (global.console && global.console.log) {
+                if (proxy.target) {
+                  global.console.log(message, proxy.target, proxy.spec);
+                } else {
+                  global.console.log(message, proxy);
+                }
+              }
+            }
 
-					promise.resolve();
-				}
-			}
+            promise.resolve();
+          }
+        }
 
-			paths = {};
-			timeout = options.timeout || defaultTimeout;
-			logCreated = makeListener('created', verbose);
+        paths = {};
+        timeout = options.timeout || defaultTimeout;
+        logCreated = makeListener('created', verbose);
 
-			function cancelPathsTimeout() {
-				clearTimeout(checkPathsTimeout);
-				checkPathsTimeout = null;
-			}
+        function cancelPathsTimeout() {
+          clearTimeout(checkPathsTimeout);
+          checkPathsTimeout = null;
+        }
 
-			function checkPaths() {
-				if (!checkPathsTimeout) return;
+        function checkPaths() {
+          if (!checkPathsTimeout) return;
 
-				var p, path;
+          var p, path;
 
-				for (p in paths) {
-					path = paths[p];
-					if (path.status !== 'ready') {
-						console.error("WIRING FAILED at " + path.status, p, path.spec);
-					}
-				}
-			}
+          for (p in paths) {
+            path = paths[p];
+            if (path.status !== 'ready') {
+              if (global.console && global.console.error) {
+                global.console.error("WIRING FAILED at " + path.status, p, path.spec);
+                global.console.error(err.stack);
+              }
+            }
+          }
+        }
 
-			checkPathsTimeout = setTimeout(checkPaths, timeout);
+        checkPathsTimeout = setTimeout(checkPaths, timeout);
 
-			plugin = {
-				create: function(promise, proxy) {
-					var path = proxy.path;
+        plugin = {
+          create: function(promise, proxy) {
+            var path = proxy.path;
 
-					if (path) {
-						paths[path] = {
-							spec: proxy.spec
-						};
-					}
-					logCreated(promise, proxy);
-				},
-				configure:  makeListener('configured', verbose),
-				initialize: makeListener('initialized', verbose),
-				ready:      makeListener('ready', true),
-				destroy:    makeListener('destroyed', true)
-			};
+            if (path) {
+              paths[path] = {
+                spec: proxy.spec
+              };
+            }
+            logCreated(promise, proxy);
+          },
+          configure:  makeListener('configured', verbose),
+          initialize: makeListener('initialized', verbose),
+          ready:      makeListener('ready', true),
+          destroy:    makeListener('destroyed', true)
+        };
 
-			return plugin;
-		}
-	};
+        return plugin;
+      }
+    };
 
-});
+  });
+})(this);
