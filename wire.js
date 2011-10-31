3bb39a17c7a2a8e6cf265b4fc3abf0335e8e7d4f
Brian Cavalier <brian@briancavalier.com>
2011-10-31 09:25:42 -0400

    Updated plugin builder to work with cram v 0.2+

--- wire.js
+++ wire.js
@@ -96,6 +96,8 @@
     // http://wiki.commonjs.org/wiki/Modules/LoaderPlugin
 
     // plugin-builder: wire/cram/builder
+    // cram v 0.2+ supports plugin-builder property
+    wire['plugin-builder'] = 'wire/cram/builder';
 
     //
     // Private functions
