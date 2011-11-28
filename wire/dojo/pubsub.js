1f7f94ed329c1f7b50d2677b89c8d75644222b47
Brian Cavalier <brian@briancavalier.com>
2011-11-28 14:15:53 -0500

    Update to latest curl release 0.5.4.  Remove now-unused function in wire/dojo/pubsub, switch to jsdoc comment style, and add a few more comments

--- wire/dojo/pubsub.js
+++ wire/dojo/pubsub.js
@@ -4,13 +4,13 @@
  * to the MIT License at: http://www.opensource.org/licenses/mit-license.php.
  */
 
-/*
-	Package: pubsub.js
-	wire plugin that sets up subscriptions and topics to be published after
-	functions are invoked, and disconnect them when an object is destroyed.
-	This implementation uses dojo.publish, dojo.subscribe and dojo.unsubscribe
-	to do the work of connecting and disconnecting event handlers.
-*/
+/**
+ * pubsub.js
+ * wire plugin that sets up subscriptions and topics to be published after
+ * functions are invoked, and disconnect them when an object is destroyed.
+ * This implementation uses dojo.publish, dojo.subscribe and dojo.unsubscribe
+ * to do the work of connecting and disconnecting event handlers.
+ */
 define(['dojo', 'aop', 'dojo/_base/connect'], function(pubsub, aop) {
 
 	return {
@@ -18,16 +18,13 @@ define(['dojo', 'aop', 'dojo/_base/connect'], function(pubsub, aop) {
 
 			var destroyHandlers = [];
 
-			/*
-				Function: proxyPublish
-				Proxies methods on target so that they publish topics after being
-				invoked.  The payload of a topic will be the return value of the method
-				that triggered it.
-
-				Parameters:
-					target - object whose methods should be proxied
-					publish - hash of method names to topics each should publish
-			*/
+            /**
+             * Proxies methods on target so that they publish topics after
+             * being invoked.  The payload of a topic will be the return
+             * value of the method that triggered it.
+             * @param target {Object} object whose methods should be proxied
+             * @param publish {Object} hash of method names to topics each should publish
+             */
 			function proxyPublish(target, publish) {
 				var remove;
 
@@ -45,27 +42,11 @@ define(['dojo', 'aop', 'dojo/_base/connect'], function(pubsub, aop) {
 				}
 			}
 
-			/*
-				Function: unproxyPublish
-				Restores the original functions on object after they've been proxied
-				by <proxyPublish> to publish topics.
-
-				Parameters:
-					object - object whose methods should be restored
-					originals - hash of method names to original methods to restore
-			*/
-			function unproxyPublish(object, originals) {
-				for(var p in originals) {
-					object[p] = originals[p];
-				}
-			}
-
 			function subscribeTarget(target, subscriptions) {
 				var subscribeHandles = [];
 				for(var topic in subscriptions) {
 					var f = subscriptions[topic];
 					if(typeof target[f] == 'function') {
-						// TODO: How to unsubscribe?
 						subscribeHandles.push(pubsub.subscribe(topic, target, f));
 					}
 				}
@@ -78,13 +59,15 @@ define(['dojo', 'aop', 'dojo/_base/connect'], function(pubsub, aop) {
 			}
 
 		    function unsubscribeTarget(handles) {
-		        for (var i = handles.length - 1; i >= 0; i--){
+		        for (var i = handles.length - 1; i >= 0; --i){
 					pubsub.unsubscribe(handles[i]);
 				}
 			}
 
+            // When the context is destroyed, remove all publish and
+            // subscribe hooks created in this context
 			destroyed.then(function onContextDestroy() {
-				for (var i = destroyHandlers.length - 1; i >= 0; i--){
+				for (var i = destroyHandlers.length - 1; i >= 0; --i){
 					destroyHandlers[i]();
 				}
 			});
