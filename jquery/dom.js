072cbbeea47f6598059f9127e9f93c4c1653d0cc
John Hann <john@e-numera.com>
2012-03-16 18:48:03 -0400

    plugin options are now pushed into cached bindings so they can be sent to adapters

--- jquery/dom.js
+++ jquery/dom.js
@@ -14,11 +14,50 @@ define(['../plugin-base/dom', 'jquery'], function(createDomPlugin, jquery) {
 
 	return createDomPlugin({
 		query: jquery,
+		first: function (selector, root) {
+			return jquery(selector, root).first();
+		},
 		addClass: function(node, cls) {
 			jquery(node).addClass(cls);
 		},
 		removeClass: function(node, cls) {
 			jquery(node).removeClass(cls);
+		},
+		placeAt: function (node, refNode, location) {
+			var $node = jquery(node);
+			// `if else` is more compressible than switch
+			if (!isNaN(location)) {
+				var $children;
+				$children = $node.siblings();
+				if (location <= 0) {
+					$node.prependTo(refNode);
+				}
+				else if (location >= children.length) {
+					$node.appendTo(refNode);
+				}
+				else {
+					$children.eq(location).before(node);
+				}
+			}
+			else if(location == 'at') {
+				jquery(refNode).empty().append(node);
+			}
+			else if(location == 'last') {
+				$node.appendTo(refNode);
+			}
+			else if(location == 'first') {
+				$node.prependTo(refNode);
+			}
+			else if(location == 'before') {
+				$node.insertBefore(refNode);
+			}
+			else if(location == 'after') {
+				$node.insertAfter(refNode);
+			}
+			else {
+				throw new Error('Unknown dom insertion command: ' + location);
+			}
+			return node;
 		}
 	});
 
