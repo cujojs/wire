18c8f73e8333a20518e139a08fea51654be8799f
John Hann <john@e-numera.com>
2012-04-25 10:28:45 -0400

    mapTokenList now converts input to string before processing

--- dom/transform/mapTokenList.js
+++ dom/transform/mapTokenList.js
@@ -2,16 +2,21 @@
 define(function () {
 "use strict";
 
+	var defaultParser, defaultDelimiter;
+
+	defaultParser = /(^|\s+)([^\s]+)/g;
+	defaultDelimiter = ' ';
+
 	return function (map, options) {
 		var parser, delimiter;
 
 		if (!options) options = {};
 
-		parser = options.parser || /(^|\s+)([^\s]+)/g;
-		delimiter = options.delimiter || ' ';
+		parser = options.parser || defaultParser;
+		delimiter = options.delimiter || defaultDelimiter;
 
 		return function translateTokenLists (tokenList) {
-			if (!tokenList) tokenList = '';
+			tokenList = '' + tokenList;
 			return tokenList.replace(parser, function (m, s, token) {
 				var trans = map[token];
 				// if there's a delimiter already (spaces, typically),
