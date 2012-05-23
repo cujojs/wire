5147eca83ff3eff4e5e4057684679f0f068b88d8
Brian Cavalier <brian@hovercraftstudios.com>
2012-05-23 12:03:08 -0400

    Simplified moduleLoader by not using has().  Returned to always including wire.load because it's not clear that define.amd will be truthy in all build environments.

--- wire.js
+++ wire.js
@@ -11,7 +11,6 @@
  *
  * @version 0.8.0
  */
-
 (function(global, define){
 define(['require', 'when', './lib/context'], function(require, when, createContext) {
 
@@ -23,10 +22,6 @@ define(['require', 'when', './lib/context'], function(require, when, createConte
 
 	rootSpec = global['wire'] || {};
 	rootOptions = { require: require };
-
-	function has() {
-		return typeof define.amd != 'undefined';
-	}
 
 	//
 	// Module API
@@ -68,34 +63,32 @@ define(['require', 'when', './lib/context'], function(require, when, createConte
 		);
 	}
 
-	if(has('amd')) {
-		/**
-		 * AMD Loader plugin API
-		 * @param name {String} spec module id, or comma-separated list of module ids
-		 * @param require {Function} loader-provide local require function
-		 * @param callback {Function} callback to call when wiring is completed. May have
-		 *  and error property that a function to call to inform the AMD loader of an error.
-		 *  See here: https://groups.google.com/forum/?fromgroups#!topic/amd-implement/u0f161drdJA
-		 */
-		wire.load = function amdLoad(name, require, callback /*, config */) {
-			// If it's a string, try to split on ',' since it could be a comma-separated
-			// list of spec module ids
-			var errback = callback.error || function(e) {
-				// Throw uncatchable exception for loaders that don't support
-				// AMD error handling.  This will propagate up to the host environment
-				setTimeout(function() { throw e; }, 0);
-			};
-
-			when(wire(name.split(','), { require: require }), callback, errback);
+	/**
+	 * AMD Loader plugin API
+	 * @param name {String} spec module id, or comma-separated list of module ids
+	 * @param require {Function} loader-provide local require function
+	 * @param callback {Function} callback to call when wiring is completed. May have
+	 *  and error property that a function to call to inform the AMD loader of an error.
+	 *  See here: https://groups.google.com/forum/?fromgroups#!topic/amd-implement/u0f161drdJA
+	 */
+	wire.load = function amdLoad(name, require, callback /*, config */) {
+		// If it's a string, try to split on ',' since it could be a comma-separated
+		// list of spec module ids
+		var errback = callback.error || function(e) {
+			// Throw uncatchable exception for loaders that don't support
+			// AMD error handling.  This will propagate up to the host environment
+			setTimeout(function() { throw e; }, 0);
 		};
 
-		/**
-		 * AMD Builder plugin API
-		 */
-		// pluginBuilder: './build/amd/builder'
-		// cram > v0.2 will support pluginBuilder property
-		wire['pluginBuilder'] = './build/amd/builder';
-	}
+		when(wire(name.split(','), { require: require }), callback, errback);
+	};
+
+	/**
+	 * AMD Builder plugin API
+	 */
+	// pluginBuilder: './build/amd/builder'
+	// cram > v0.2 will support pluginBuilder property
+	wire['pluginBuilder'] = './build/amd/builder';
 
 	return wire;
 
