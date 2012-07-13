628d096edea7f08ff4fb339c0f7a798a374c3896
Brian Cavalier <brian@hovercraftstudios.com>
2012-07-13 16:16:55 -0400

    Split startup lifecycle into 2 phases and allow components to become resolvable after the init phase.  This allows circular deps to be resolved in any facets that happen after init

---
+++ test/node/circular-refs.js
@@ -0,0 +1,84 @@
+(function(buster, timeout, wire, plugin) {
+"use strict";
+
+var assert, refute, fail;
+
+assert = buster.assert;
+refute = buster.refute;
+fail = buster.assertions.fail;
+
+buster.testCase('circular-refs', {
+	setUp: function() {
+		// Setup a plugin that will record lifecycle steps
+		plugin.wire$plugin = function() {
+			return {
+				facets: {
+					shouldResolve: {
+						'connect:before': function(resolver, facet, wire) {
+							wire.resolveRef(facet.options).then(resolver.resolve, resolver.reject);
+						}
+					},
+					shouldNotResolve: {
+						'initialize:after': function(resolver, facet, wire) {
+							wire.resolveRef(facet.options).then(resolver.reject, resolver.resolve);
+						}
+					}
+				}
+			}
+		};
+	},
+
+	tearDown: function() {
+		// Remove the plugin
+		// Since this is a cached plugin
+		delete plugin.wire$plugin;
+	},
+
+	'should resolve circular deps after init has finished': function(done) {
+		var promise = timeout(wire({
+			component1: {
+				module: './test/node/fixtures/object',
+				shouldResolve: 'component2'
+			},
+			component2: {
+				module: './test/node/fixtures/object',
+				shouldResolve: 'component1'
+			}
+		}), 100);
+
+		promise.then(
+			function(context) {
+				assert.defined(context.component1);
+				assert.defined(context.component2);
+			},
+			fail
+		).always(done);
+	},
+
+	'should not resolve circular deps before init has finished': function(done) {
+		var promise = timeout(wire({
+			component1: {
+				module: './test/node/fixtures/object',
+				shouldNotResolve: 'component2'
+			},
+			component2: {
+				module: './test/node/fixtures/object',
+				shouldNotResolve: 'component1'
+			}
+		}), 100);
+
+		promise.then(
+			fail,
+			function() {
+				assert(true);
+			}
+		).always(done);
+	}
+});
+
+})(
+	require('buster'),
+	require('when/timeout'),
+	require('../..'),
+	require('./fixtures/object')
+);
--- test/node/lifecycle.js
+++ test/node/lifecycle.js
@@ -1,71 +1,71 @@
 (function(buster, wire, plugin) {
-"use strict";
+	"use strict";
 
-var assert, refute, fail, steps;
+	var assert, refute, fail, steps;
 
-assert = buster.assert;
-refute = buster.refute;
-fail = buster.assertions.fail;
+	assert = buster.assert;
+	refute = buster.refute;
+	fail = buster.assertions.fail;
 
-steps = ['create', 'configure', 'initialize', 'connect', 'ready', 'destroy'].reduce(
-	function(lifecycle, step) {
-		lifecycle.push(step + ':before');
-		lifecycle.push(step);
-		lifecycle.push(step + ':after');
-		return lifecycle;
-	}, []
-);
+	steps = ['create', 'configure', 'initialize', 'connect', 'ready', 'destroy'].reduce(
+		function(lifecycle, step) {
+			lifecycle.push(step + ':before');
+			lifecycle.push(step);
+			lifecycle.push(step + ':after');
+			return lifecycle;
+		}, []
+	);
 
-buster.testCase('lifecycle', {
-	setUp: function() {
-		// Setup a plugin that will record lifecycle steps
-		plugin.wire$plugin = function() {
-			var instance, order;
+	buster.testCase('lifecycle', {
+		setUp: function() {
+			// Setup a plugin that will record lifecycle steps
+			plugin.wire$plugin = function() {
+				var instance, order;
 
-			instance = {};
-			order = 0;
+				instance = {};
+				order = 0;
 
-			steps.forEach(function(step) {
-				instance[step] = function(resolver, proxy) {
-					if(!proxy.target.lifecycle) proxy.target.lifecycle = [];
-					proxy.target.lifecycle.push(step);
-					resolver.resolve();
-				}
-			});
+				steps.forEach(function(step) {
+					instance[step] = function(resolver, proxy) {
+						if(!proxy.target.lifecycle) proxy.target.lifecycle = [];
+						proxy.target.lifecycle.push(step);
+						resolver.resolve();
+					}
+				});
 
-			return instance;
-		};
-	},
+				return instance;
+			};
+		},
 
-	tearDown: function() {
-		// Remove the plugin
-		// Since this is a cached plugin
-		delete plugin.wire$plugin;
-	},
+		tearDown: function() {
+			// Remove the plugin
+			// Since this is a cached plugin
+			delete plugin.wire$plugin;
+		},
 
-	'should process components in lifecycle step order': function(done) {
-		wire({
-			component: { module: './test/node/fixtures/object' }
-		}).then(
-			function(context) {
-				var component = context.component;
+		'should process components in lifecycle step order': function(done) {
+			wire({
+				component: { module: './test/node/fixtures/object' }
+			}).then(
+				function(context) {
+					var component = context.component;
 
-				// Ensure that create thru ready happen in order, and that
-				// destroy does not happen
-				assert.equals(component.lifecycle, steps.slice(0, steps.length-3));
+					// Ensure that create thru ready happen in order, and that
+					// destroy does not happen
+					assert.equals(component.lifecycle, steps.slice(0, steps.length-3));
 
-				// Ensure that destroy happens and is always last
-				context.destroy().then(
-					function() {
-						assert.equals(component.lifecycle, steps);
-					},
-					fail
-				).then(done, done);
-			},
-			fail
-		);
-	}
-})
+					// Ensure that destroy happens and is always last
+					return context.destroy().then(
+						function() {
+							assert.equals(component.lifecycle, steps);
+						},
+						fail
+					);
+				},
+				fail
+			).always(done);
+		}
+	});
 
 })(
 	require('buster'),
