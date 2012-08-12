fe4b9acd51b47f5b01e5348a71b5ac117688b7b2
Brian Cavalier <brian@hovercraftstudios.com>
2012-08-11 21:25:54 -0400

    toggleClasses unit tests for cases where node or classes are bound

--- test/node/dom/transform/toggleClasses.js
+++ test/node/dom/transform/toggleClasses.js
@@ -18,37 +18,98 @@ buster.testCase('dom/transform/toggleClasses', {
 	'add': {
 		'should add classes if not present': function() {
 			var t = toggleClasses();
-			assert.equals(t.add(fakeNode(), 'test').className, 'test');
+			assert.equals(t.add(fakeNode(), 'a').className, 'a');
 		},
 
 		'should not add duplicate classes': function() {
 			var t = toggleClasses();
-			assert.equals(t.add(fakeNode(), 'test').className, 'test');
+			assert.equals(t.add(fakeNode('a'), 'a').className, 'a');
+		},
+
+		'when node is bound': {
+			'should add class to bound node': function() {
+				var n, t;
+				n = fakeNode();
+				t = toggleClasses({ node: n });
+
+				assert.equals(t.add('a').className, 'a');
+			}
+		},
+
+		'when classes are bound': {
+			'should add bound classes': function() {
+				var t = toggleClasses({ classes: 'a' });
+				assert.equals(t.add(fakeNode()).className, 'a');
+			}
 		}
+
 	},
 
 	'remove': {
 		'should remove classes if present': function() {
 			var t = toggleClasses();
-			assert.equals(t.remove(fakeNode('test'), 'test').className, '');
+			assert.equals(t.remove(fakeNode('a'), 'a').className, '');
 		},
 
 		'should be a noop if classes not present': function() {
 			var t = toggleClasses();
 			assert.equals(t.remove(fakeNode('a'), 'b').className, 'a');
+		},
+
+		'when node is bound': {
+			'should remove class from bound node': function() {
+				var n, t;
+				n = fakeNode('test');
+				t = toggleClasses({ node: n });
+
+				assert.equals(t.remove('test').className, '');
+			}
+		},
+
+		'when classes are bound': {
+			'should remove bound classes': function() {
+				var t = toggleClasses({ classes: 'a' });
+				assert.equals(t.remove(fakeNode('a')).className, '');
+			}
 		}
+
 	},
 
 	'toggle': {
 		'should add classes if not present': function() {
-			var t = toggleClasses();
-			assert.equals(t(fakeNode(), 'a').className, 'a');
+			var toggle = toggleClasses();
+			assert.equals(toggle(fakeNode(), 'a').className, 'a');
 		},
 
 		'should remove classes if present': function() {
-			var t = toggleClasses();
-			assert.equals(t(fakeNode('a'), 'a').className, '');
+			var toggle = toggleClasses();
+			assert.equals(toggle(fakeNode('a'), 'a').className, '');
+		},
+
+		'when node is bound': {
+			'should toggle class on bound node': function() {
+				var n, toggle;
+
+				n = fakeNode();
+				toggle = toggleClasses({ node: n });
+
+				assert.equals(toggle('a').className, 'a');
+				assert.equals(toggle('a').className, '');
+			}
+		},
+
+		'when classes are bound': {
+			'should toggle bound classes': function() {
+				var n, toggle;
+
+				n = fakeNode();
+				toggle = toggleClasses({ classes: 'a' });
+
+				assert.equals(toggle(n).className, 'a');
+				assert.equals(toggle(n).className, '');
+			}
 		}
+
 	}
 
 });
