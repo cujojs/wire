e6f5732f5ffd6db0e0f7e7f3066e295a83880c44
Brian Cavalier <brian@hovercraftstudios.com>
2012-11-08 09:48:02 -0500

    Close #58. Unit tests for wire/debug setting constructor name

---
+++ test/node/debug.js
@@ -0,0 +1,58 @@
+(function(buster, wire) {
+'use strict';
+
+var assert, refute, fail;
+
+assert = buster.assert;
+refute = buster.refute;
+fail = buster.assertions.fail;
+
+buster.testCase('wire/debug', {
+	'should set constructor on constructor-less object components': function(done) {
+		wire({
+			debug: { module: './debug' },
+			myComponent: {
+				create: './test/node/fixtures/object'
+			}
+		}).then(function(context) {
+			assert.isFunction(context.myComponent.constructor);
+			assert.equals(context.myComponent.constructor.name, 'myComponent');
+		}).then(done, done);
+	},
+
+	'should set not constructor on components that already have one': function(done) {
+		wire({
+			debug: { module: './debug' },
+			myComponent: {
+				create: './test/node/fixtures/constructor'
+			}
+		}).then(function(context) {
+			assert.isFunction(context.myComponent.constructor);
+			refute.equals(context.myComponent.constructor.name, 'myComponent');
+		}).then(done, done);
+	},
+
+	'should not set constructor on non-object components': function(done) {
+		wire({
+			debug: { module: './debug' },
+			myComponent: 'just a string'
+		}).then(function(context) {
+			refute.equals(context.myComponent.constructor.name, 'myComponent');
+		}).then(done, done);
+	},
+
+	'should not set constructor when not active': function(done) {
+		wire({
+			myComponent: {
+				create: './test/node/fixtures/object'
+			}
+		}).then(function(context) {
+			refute.equals(context.myComponent.constructor.name, 'myComponent');
+		}).then(done, done);
+	}
+
+});
+})(
+	require('buster'),
+	require('../..')
+);
