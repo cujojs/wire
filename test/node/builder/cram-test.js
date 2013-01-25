0e83afc9a737e5cb666d62c7d94449223caf30c1
unscriptable <john@unscriptable.com>
2013-01-25 10:23:50 -0500

    update tests to latest cram plugin

--- test/node/builder/cram-test.js
+++ test/node/builder/cram-test.js
@@ -14,17 +14,20 @@ buster.testCase('wire/builder/cram', {
 	'should write a named define': function(done) {
 		var spec = {};
 
-		function req() {
-			return spec;
+		function req(ids, cb) {
+			cb(spec);
 		}
 
-		builder.compile('wire!test', req, {
+		builder.compile('wire', 'test', req, {
 			read: function(_, cb) {
-				cb(JSON.stringify(spec));
+				cb(specObjectToModule(spec));
 			},
 			write: function(content) {
 				assert(/^define\("test",[\s\S]+\);$/.test(content));
 				done();
+			},
+			error: function (reason) {
+				refute(true, reason);
 			}
 		});
 	},
@@ -41,18 +44,21 @@ buster.testCase('wire/builder/cram', {
 			deps.push('"' + name + '"');
 		});
 
-		function req() {
-			return spec;
+		function req(ids, cb) {
+			cb(spec);
 		}
 
-		builder.compile('wire!test', req, {
+		builder.compile('wire', 'test', req, {
 			read: function(_, cb) {
-				cb(JSON.stringify(spec));
+				cb('define(' + JSON.stringify(spec) +');');
 			},
 			write: function(content) {
-				var str = '[' + ['"wire"'].concat(deps).join(',') + ']';
+				var str = '[' + ['"wire"'].concat(deps).join(', ') + ']';
 				refute.equals(content.indexOf(str), -1);
 				done();
+			},
+			error: function (reason) {
+				refute(true, reason);
 			}
 		});
 	},
@@ -66,17 +72,20 @@ buster.testCase('wire/builder/cram', {
 
 		json = JSON.stringify(spec);
 
-		function req() {
-			return spec;
+		function req(ids, cb) {
+			cb(spec);
 		}
 
-		builder.compile('wire!test', req, {
+		builder.compile('wire', 'test', req, {
 			read: function(_, cb) {
-				cb(json);
+				cb(specObjectToModule(spec));
 			},
 			write: function(content) {
 				refute.equals(content.indexOf(json), -1);
 				done();
+			},
+			error: function (reason) {
+				refute(true, reason);
 			}
 		});
 	},
@@ -84,46 +93,56 @@ buster.testCase('wire/builder/cram', {
 	'with a comma-separated list of specs': {
 		'should generate a define for each': function(done) {
 			var specs = {
-				atest: { a: { module: 'a' } },
-				btest: { b: { module: 'b' } }
+				'atest.js': { a: { module: 'a' } },
+				'btest.js': { b: { module: 'b' } }
 			};
 
-			function req(moduleId) {
-				return specs[moduleId];
+			function req(moduleId, cb) {
+				cb(specs[moduleId]);
 			}
 
-			builder.compile('wire!atest,btest', req, {
+			builder.compile('wire', 'atest,btest', req, {
 				read: function(path, cb) {
-					cb(JSON.stringify(specs[path]));
+					cb(specObjectToModule(specs[path]));
 				},
 				write: function(content) {
 					assert(/^define\("atest",[\s\S]+\);\s*define\("btest",[\s\S]+\);$/.test(content));
 					done();
+				},
+				error: function (reason) {
+					refute(true, reason);
 				}
 			});
 		},
 
 		'should generate defines with original content for each': function(done) {
 			var specs = {
-				atest: { a: { module: 'a' } },
-				btest: { b: { module: 'b' } }
+				'atest.js': { a: { module: 'a' } },
+				'btest.js': { b: { module: 'b' } }
 			};
 
-			function req(moduleId) {
-				return specs[moduleId];
+			function req(moduleId, cb) {
+				cb(specs[moduleId]);
 			}
 
-			builder.compile('wire!atest,btest', req, {
+			builder.compile('wire', 'atest,btest', req, {
 				read: function(path, cb) {
-					cb(JSON.stringify(specs[path]));
+					cb(specObjectToModule(specs[path]));
 				},
 				write: function(content) {
-					refute.equals(content.indexOf(JSON.stringify(specs.atest)), -1);
-					refute.equals(content.indexOf(JSON.stringify(specs.btest)), -1);
+					refute.equals(content.indexOf(JSON.stringify(specs['atest.js'])), -1);
+					refute.equals(content.indexOf(JSON.stringify(specs['btest.js'])), -1);
 					done();
+				},
+				error: function (reason) {
+					refute(true, reason);
 				}
 			});
 		}
 
 	}
 });
+
+function specObjectToModule (spec) {
+	return 'define(' + JSON.stringify(spec) + ');'
+}
