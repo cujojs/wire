ead569a29fc9222caf9561f6f2b6516565fee9db
Brian Cavalier <brian@hovercraftstudios.com>
2013-03-18 13:11:28 -0400

    Remove context destroyers test, since destroyers have been removed

--- test/node/context.js
+++ test/node/context.js
@@ -51,60 +51,6 @@ buster.testCase('context', {
 		}
 	},
 
-	'destroyers': {
-		'//should execute when context is destroyed': function(done) {
-			var executed = false;
-			createContext({}, null, {
-				require: require,
-				contextHandlers: {
-					destroy: function() { executed = true; }
-				}
-			}).then(
-				function(context) {
-					refute(executed);
-
-					context.destroy().then(
-						function() {
-							assert(executed);
-						}
-					);
-				}
-			).then(done, done);
-		}
-	},
-
-	'initializers and destroyers': {
-		'//should execute in correct order': function(done) {
-		var init, destroy;
-			createContext({}, null, {
-				require: require,
-				contextHandlers: {
-					init: function() {
-						refute(init);
-						refute(destroy);
-						init = true;
-					},
-					destroy: function() {
-						assert(init);
-						refute(destroy);
-						destroy = true;
-					}
-				}
-			}).then(
-				function(context) {
-					// Should not have executed yet
-					refute(destroy);
-
-					return context.destroy().then(
-						function() {
-							assert(destroy);
-						}
-					);
-				}
-			).then(done, done);
-		}
-	},
-
 	'lifecycle': {
 		'destroy': {
 			'tearDown': function() {
