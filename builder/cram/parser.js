67178e2ab0c088ef4f462c714bc3ced881c74056
fabricematrat <fabricematrat@gmail.com>
2013-11-05 21:20:28 +0100

    added test

---
+++ builder/cram/parser.js
@@ -0,0 +1,47 @@
+/** @license MIT License (c) copyright F. Matrat */
+
+/**
+ * TODO Fabrice.
+ * https://github.com/cujojs/cram
+ *
+ * wire is part of the cujo.js family of libraries (http://cujojs.com/)
+ *
+ * Licensed under the MIT License at:
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+(function(define) {
+define(function(require) {
+
+    var replaceIdsRx, removeCommentsRx;
+
+    // adapted from cram's scan function:
+    //replaceIdsRegex = /(define)\s*\(\s*(?:\s*["']([^"']*)["']\s*,)?(?:\s*\[([^\]]+)\]\s*,)?\s*(function)?\s*(?:\(([^)]*)\))?/g;
+    replaceIdsRx = /(define)\s*\(\s*(?:\s*["']([^"']*)["']\s*,)?(?:\s*\[([^]]*)]\s*,)?/;
+    //removeCommentsRx = /define\(([\s*\/\/|\/\*].*)[\s\S]*\{/g;
+    //removeCommentsRx = /define\([\{\}\/\w\S\s]*\*\/\n\r\{/g;
+    removeCommentsRx = /define\([\{\}\/\w\S\s]*\*\/\n\{/g;
+    return {
+        injectIds: injectIds
+    };
+
+    function injectIds(moduleText, absId, moduleIds) {
+        // note: replaceIdsRx removes commas, parens, and brackets
+        return moduleText.replace(removeCommentsRx, 'define({')
+            .replace(replaceIdsRx, function(m, def, mid, depIds) {
+                // merge deps, but not args since they're not referenced in module
+                if (depIds) moduleIds = moduleIds.concat(depIds);
+
+                moduleIds = moduleIds.map(quoted).join(', ');
+                if (moduleIds) moduleIds = '[' + moduleIds + '], ';
+
+                return def + '(' + quoted(absId) + ', ' + moduleIds;
+            }
+        );
+    }
+
+    function quoted(id) {
+        return '"' + id + '"';
+    }
+
+});
+}(typeof define === 'function' && define.amd ? define : function(factory) { module.exports = factory(require); }));
---
+++ test/node/builder/cram/parser-test.js
@@ -0,0 +1,81 @@
+var buster, assert, refute, fail, builder, forEach;
+
+buster = require('buster');
+assert = buster.assert;
+refute = buster.refute;
+fail = buster.assertions.fail;
+
+parser = require('../../../../builder/cram/parser');
+
+forEach = Array.prototype.forEach.call.bind(Array.prototype.forEach);
+
+buster.testCase('wire/builder/cram/parser', {
+      /*
+    'should write a named define': function(done) {
+        var spec, test;
+        test = "test"
+        spec ="{\n\ra: { test: 'a' }\n\r}";
+        assert.equals(parser.injectIds(specObjectToModule(spec), test, []), specObjectToModuleWithIds(spec, test));
+        done();
+    },
+    'should write a named define even with a one line comment': function(done) {
+        var spec, test, result;
+        test = "test"
+        spec = "{\n\r";
+        result = "{\n\r";
+        spec += "//This is my comment\n\r";
+        spec += "a: { test: 'a' }}";
+        result += "a: { test: 'a' }}";
+        assert.equals(parser.injectIds(specObjectToModule(spec), test, []).replace(/(\n|\r)/g,''), specObjectToModuleWithIds(result, test).replace(/(\n|\r)/g,''));
+        done();
+    },
+    'should write a named define even with a comment at the end of an existing line': function(done) {
+        var spec, test, result;
+        test = "test"
+        spec = "{\n\r";
+        result = "{\n\r";
+        spec += "a: { test: 'a' }//This is my comment\n\r}";
+        result += "a: { test: 'a' }}";
+        assert.equals(parser.injectIds(specObjectToModule(spec), test, []).replace(/(\n|\r)/g,''), specObjectToModuleWithIds(result, test).replace(/(\n|\r)/g,''));
+        done();
+    },
+    'should write a named define even with a comment at the end of the first line': function(done) {
+        var spec, test, result;
+        test = "test"
+        spec = "{//This is my comment\n\r";
+        result = "{\n\r";
+        spec += "a: { test: 'a' }}";
+        result += "a: { test: 'a' }}";
+        assert.equals(parser.injectIds(specObjectToModule(spec), test, []).replace(/(\n|\r)/g,''), specObjectToModuleWithIds(result, test).replace(/(\n|\r)/g,''));
+        done();
+    },
+    'should write a named define even with a comment inbetween': function(done) {
+        var spec, test, result;
+        test = "test"
+        spec = "/*This is my comment*/ //{\n\r";
+//        result = "{\n\r";
+//        spec += "a: { test: 'a' }}";
+//        result += "a: { test: 'a' }}";
+//        assert.equals(parser.injectIds(specObjectToModule(spec), test, []).replace(/(\n|\r)/g,''), specObjectToModuleWithIds(result, test).replace(/(\n|\r)/g,''));
+//        done();
+//    },
+    'should write a named define even if one value contains a comment like but is not a comment': function(done) {
+        var spec, test, result;
+        test = "test"
+        spec = "/*toto*/ {\n\r";
+        result = "{\n\r";
+        spec += "a: { test: 'http://example.com' }\n\r}";
+        result += "a: { test: 'http://example.com' }\n\r}";
+        console.log(parser.injectIds(specObjectToModule(spec), test, []).replace(/(\n|\r)/g,''));
+        console.log(specObjectToModuleWithIds(result, test).replace(/(\n|\r)/g,''));
+        assert.equals(parser.injectIds(specObjectToModule(spec), test, []).replace(/(\n|\r)/g,''), specObjectToModuleWithIds(result, test).replace(/(\n|\r)/g,''));
+        done();
+    }
+});
+
+function specObjectToModule(spec) {
+    return 'define(' + spec + ');';
+}
+function specObjectToModuleWithIds(spec, id) {
+    return 'define("' + id + '", ' + spec + ');';
+}
