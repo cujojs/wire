b123f54b3c7328537510ed085b65a18bdae5bd51
Krzysztof Chrapka <krzysztofchrapka@rage-banana>
2016-04-10 12:06:26 +0200

    Use spread operator instead of apply when supported
    Closes #181

--- test/buster.js
+++ test/buster.js
@@ -1,8 +1,42 @@
+'use strict';
+
 require('gent/test-adapter/buster');
+
+function evaluates (statement) {
+	try {
+		/* jshint evil: true */
+		eval(statement);
+		/* jshint evil: false */
+		return true;
+	} catch (err) {
+		return false;
+	}
+}
+
+function isClassAvailable() {
+	return evaluates('class es6TestClass_ibyechBaloodren7 {}');
+}
+
+function isSpreadAvailable() {
+	return evaluates('parseInt(...["20", 10])');
+}
+
+var tests = ['node/**/*-test.js'];
+
+console.log('class operator %savailable', isClassAvailable() ? '' : 'not ');
+console.log('spread operator %savailable', isSpreadAvailable() ? '' : 'not ');
+
+if(
+	isClassAvailable()
+	&& isSpreadAvailable()
+	&& !('ES_VERSION' in process.env && parseFloat(process.env.ES_VERSION) < 6)
+) {
+	tests.push('node-es6/**/*-test.js');
+}
 
 module.exports['node'] = {
 	environment: 'node',
-	tests: ['node/**/*-test.js']
+	tests: tests
 	// TODO: Why doesn't this work?
 	//, testHelpers:['gent/test-adapter/buster']
 };
---
+++ test/node-es6/lib/plugin/basePlugin-test.js
@@ -0,0 +1,95 @@
+/* jshint esversion: 6 */
+(function(buster, context) {
+'use strict';
+
+var assert, refute, fail, sentinel;
+
+assert = buster.assert;
+refute = buster.refute;
+fail = buster.fail;
+
+sentinel = {};
+
+function createContext(spec) {
+	return context.call(null, spec, null, { require: require });
+}
+
+class es6Class
+{
+	constructor () {
+		this.constructorRan = true;
+		this.args = Array.prototype.slice.call(arguments);
+	}
+
+	someMethod() {
+
+	}
+}
+
+buster.testCase('es6/lib/plugin/basePlugin', {
+	'clone factory': {
+		'should call constructor when cloning an object with an es6 constructor': function() {
+			class FabulousEs6 {
+				constructor () {
+					this.instanceProp = 'instanceProp';
+				}
+			}
+			FabulousEs6.prototype.prototypeProp = 'prototypeProp';
+
+			return createContext({
+				fab: {
+					create: FabulousEs6
+				},
+				copy: {
+					clone: { $ref: 'fab' }
+				}
+			}).then(
+				function(context) {
+					assert.defined(context.copy, 'copy is defined');
+					assert.defined(context.copy.prototypeProp, 'copy.prototypeProp is defined');
+					assert.defined(context.copy.instanceProp, 'copy.instanceProp is defined');
+					refute.same(context.copy, context.fab);
+				},
+				fail
+			);
+		}
+	},
+
+	'create factory': {
+		'should call es6 constructor': function() {
+			return createContext({
+				test: {
+					create: {
+						module: es6Class,
+					}
+				}
+			}).then(
+				function(context) {
+					assert(context.test.constructorRan);
+				},
+				fail
+			);
+		},
+
+		'should call es6 constructor functions with args': function() {
+			return createContext({
+				test: {
+					create: {
+						module: es6Class,
+						args: [1, 'foo', 1.7]
+					}
+				}
+			}).then(
+				function(context) {
+					assert(context.test instanceof es6Class);
+					assert.equals(context.test.args, [1, 'foo', 1.7]);
+				},
+				fail
+			);
+		},
+	}
+});
+})(
+	require('buster'),
+	require('../../../../lib/context')
+);
